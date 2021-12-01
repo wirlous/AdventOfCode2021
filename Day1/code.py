3e6ec01426dda358493f9a97ed588331229a870a
Carlos Fajardo <cfajardosanchez@gmail.com>
2021-12-01 23:04:41 +0100

    Add Day 1 Solution

---
+++ Day1/code.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 21:22:43 2021
+
+@author: fajardo
+"""
+
+
+def readInput():
+    data = []
+    with open("input.txt", 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            data.append(int(line.strip()))
+    return data
+
+    
+def getNumIncrements(data, n):
+    assert(len(data) > n)
+    count = 0
+    prevValue = sum(data[0:n])
+    for i in range(1, len(data)-n+1):
+        value = sum(data[i:i+n])
+        if (value > prevValue):
+            count = count + 1;
+        prevValue = value
+    return count
+
+if __name__ == "__main__":
+    data = readInput()
+    #data = [199,200,208,210,200,207,240,269,260,263] # Test data
+    n = 1
+    count = getNumIncrements(data, n)
+    print("Increment window {} = {}".format(3, count)) #1521
+    
+    n = 3
+    count = getNumIncrements(data, n)
+    print("Increment window {} = {}".format(n, count)) #1543
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AdventOfCode2021
 Advent code 2021
+
+Solutions to Advent Of Code 2021
+https://adventofcode.com/2021
